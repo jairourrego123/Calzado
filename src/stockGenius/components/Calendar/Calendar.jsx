6c99c886f73401775666fc1c8f084179d79eb19d
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2024-03-03 13:22:35 -0500

    calendar

---
+++ src/stockGenius/components/Calendar/Calendar.jsx
@@ -0,0 +1,50 @@
+import {useState} from 'react'
+import './Calendar.css'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import es from 'date-fns/locale/es';
+
+function CalendarStyle(vale) {
+    
+}
+
+function Calendar() {
+
+    const [date, setDate] = useState(new Date());
+    
+    const handleDateChange = (selectedDate) => {
+      if (selectedDate > new Date()) {
+         return;
+      }
+  
+      setDate(selectedDate);
+    }
+    return (
+        
+
+        <div className='stock-genius-date'> 
+            
+            <DatePicker
+                showIcon
+                locale={es}
+                selected={date}
+                onChange={handleDateChange}
+                dateFormat="dd/MM/yyyy h:mm aa" 
+                toggleCalendarOnIconClick
+                // customInput={}
+                // minDate={new Date()}
+                maxDate={new Date()}
+                icon={<img className='stock-genius-calendar-icon' src='/assets/icons/calendar.svg'/>
+                }
+                // withPortal
+                className='stock-genius-calendar'
+                // readOnly 
+            />
+        
+        </div>
+
+
+    )
+}
+
+export default Calendar
