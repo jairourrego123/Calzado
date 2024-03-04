8949301fd202da1ac774902ba7d3444e69dd1b57
JAIRO MILLER URREGO GARAY <jairourrego123@gmail.com>
2024-03-04 15:26:54 -0500

    component table

--- src/stockGenius/components/Table/Table.jsx
+++ src/stockGenius/components/Table/Table.jsx
@@ -1,40 +1,27 @@
 import './Table.css'
-function Table({}) {
+function Table({data,styles}) {
+
+  if (!data || data.length === 0) {
+    return <div>No hay datos disponibles</div>;
+  }
+  const columns = Object.keys(data[0]); // Obtiene las columnas a partir de las claves del primer objeto
+
   return (
-    <table>
+    <table className='stock-genius-component-table'>
     <thead>
       <tr>
-        <th scope="col">Account</th>
-        <th scope="col">Due Date</th>
-        <th scope="col">Amount</th>
-        <th scope="col">Period</th>
+        {columns.map((column,index)=> <th key={index} scope='col'>{column}</th>)}
+       
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td data-label="Account">Visa - 3412</td>
-        <td data-label="Due Date">04/01/2016</td>
-        <td data-label="Amount">$1,190</td>
-        <td data-label="Period">03/01/2016 - 03/31/2016</td>
-      </tr>
-      <tr>
-        <td  data-label="Account">Visa - 6076</td>
-        <td data-label="Due Date">03/01/2016</td>
-        <td data-label="Amount">$2,443</td>
-        <td data-label="Period">02/01/2016 - 02/29/2016</td>
-      </tr>
-      <tr>
-        <td  data-label="Account">Corporate AMEX</td>
-        <td data-label="Due Date">03/01/2016</td>
-        <td data-label="Amount">$1,181</td>
-        <td data-label="Period">02/01/2016 - 02/29/2016</td>
-      </tr>
-      <tr>
-        <td  data-label="Acount">Visa - 3412</td>
-        <td data-label="Due Date">02/01/2016</td>
-        <td data-label="Amount">$842</td>
-        <td data-label="Period">01/01/2016 - 01/31/2016</td>
-      </tr>
+    {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {columns.map((column, columnIndex) => (
+              <td key={columnIndex} data-label={column}>{row[column]}</td>
+            ))}
+          </tr>))}
+
     </tbody>
   </table>
   )
