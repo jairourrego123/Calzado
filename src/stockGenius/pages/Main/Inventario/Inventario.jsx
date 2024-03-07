0498430290ba60ddc4f71e6fbf1bdce2f1f6ff2d
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2024-03-07 15:58:02 -0500

    page inventario

--- src/stockGenius/pages/Main/Inventario/Inventario.jsx
+++ src/stockGenius/pages/Main/Inventario/Inventario.jsx
@@ -1,8 +1,16 @@
 import Header from "../../../components/Header/Header"
+import Search from "../../../components/Search/Search"
+import TableWithCheckbox from "../../../components/TableWithCheckbox/TableWithCheckbox"
+import Layout from "../../../components/Layout/Layout"
 
 function Inventario() {
   return (
+    <>
     <Header title={"Inventario"}/>
+    <Search/>
+    <Layout/>
+    <TableWithCheckbox/>
+    </>
   )
 }
 
