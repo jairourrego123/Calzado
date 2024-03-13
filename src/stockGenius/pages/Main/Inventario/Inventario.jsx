483d214405c8c4af0737894c9120061e311c0064
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2024-03-13 08:09:40 -0500

    pages

--- src/stockGenius/pages/Main/Inventario/Inventario.jsx
+++ src/stockGenius/pages/Main/Inventario/Inventario.jsx
@@ -2,14 +2,35 @@ import Header from "../../../components/Header/Header"
 import Search from "../../../components/Search/Search"
 import TableWithCheckbox from "../../../components/TableWithCheckbox/TableWithCheckbox"
 import Layout from "../../../components/Layout/Layout"
+import Mostrar from "../../../components/Mostrar/Mostrar"
+import Delete from "../../../components/Delete/Delete"
+import Icon from "../../../components/Icon/Icon"
+
+import './Inventario.css'
 
 function Inventario() {
   return (
     <>
-    <Header title={"Inventario"}/>
-    <Search/>
-    <Layout/>
-    <TableWithCheckbox/>
+      <Header title={"Inventario"} />
+
+      <Search />
+
+      <div className="stock-genius-inventario-layoth">
+
+        <div className="stock-genius-inventario-layoth-left">
+            <Mostrar />
+
+          <select className="stock-genius-options" id="opciones" name="opciones">
+            <option value="1">Disponible</option>
+            <option value="0">Fuera de Stock</option>
+          </select>
+
+
+          <Delete />
+        </div>
+          <Icon icon={"add"} />
+      </div>
+      <TableWithCheckbox />
     </>
   )
 }
