0897a6812afed2caceb5866781dfdd0005586cdc
jairourrego123 <jairourrego123@gmail.com>
2024-03-13 17:16:12 -0500

    pages

--- src/stockGenius/pages/Main/Home/Home.jsx
+++ src/stockGenius/pages/Main/Home/Home.jsx
@@ -1,23 +1,86 @@
-import Card from '../../../components/Card/Card'
-import Header from '../../../components/Header/Header'
-import Table from '../../../components/Table/Table'
-import './Home.css'
+import Card from "../../../components/Card/Card";
+import Header from "../../../components/Header/Header";
+import Table from "../../../components/Table/Table";
+import config from '../../../const/config.json'
+
+import "./Home.css";
 function Home() {
-  const data = [{ "comprador": "jairo Miller  Urrego", "cantidad": "20", "precio": "120.000", "fecha": "10/03/2024" }, { "comprador": "jairo Miller  Urrego", "cantidad": "20", "precio": "120.000", "fecha": "10/03/2024" }, { "comprador": "jairo Miller  Urrego", "cantidad": "20", "precio": "120.000", "fecha": "10/03/2024" }, { "comprador": "jairo Miller  Urrego", "cantidad": "20", "precio": "120.000", "fecha": "10/03/2024" }]
+  const data = [
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+  ];
 
   return (
-    <div className='stock-genius-home-container'>
-      <div className='stock-genius-home-header'><Header title={"Home"} /></div>
+    <div className="stock-genius-home-container">
+      <div className="stock-genius-home-header" style={{backgroundColor:config.backgroundPrincipal}}>
+        <Header title={"Home"} />
+      </div>
 
       <span className="stock-genius-home-layout">Últimas ventas</span>
-      <div className='stock-genius-home-table'><Table data={data} /></div>
-      <div className='stock-genius-home-cards'>
-        <Card icon={"cash-card"} text={"Ventas del dia"} value={"$50.000.000"} />
-        <Card icon={"cash-card"} text={"Ganancias del dia"} value={"$2.000.000"} />
+      <div className="stock-genius-home-table">
+        <Table data={data} />
+      </div>
+      <div className="stock-genius-home-cards">
+        <Card
+          icon={"cash-card"}
+          text={"Ventas del dia"}
+          value={"$50.000.000"}
+        />
+        <Card
+          icon={"cash-card"}
+          text={"Ganancias del dia"}
+          value={"$2.000.000"}
+        />
         <Card icon={"cash-card"} text={"Abonos del dia"} value={"$1.999.000"} />
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/stockGenius/pages/Main/Inventario/Inventario.jsx
+++ src/stockGenius/pages/Main/Inventario/Inventario.jsx
@@ -1,38 +1,112 @@
-import Header from "../../../components/Header/Header"
-import Search from "../../../components/Search/Search"
-import TableWithCheckbox from "../../../components/TableWithCheckbox/TableWithCheckbox"
-import Layout from "../../../components/Layout/Layout"
-import Mostrar from "../../../components/Mostrar/Mostrar"
-import Delete from "../../../components/Delete/Delete"
-import Icon from "../../../components/Icon/Icon"
-
-import './Inventario.css'
+import Header from "../../../components/Header/Header";
+import Search from "../../../components/Search/Search";
+import TableWithCheckbox from "../../../components/TableWithCheckbox/TableWithCheckbox";
+import Mostrar from "../../../components/Mostrar/Mostrar";
+import Delete from "../../../components/Delete/Delete";
+import Icon from "../../../components/Icon/Icon";
+import config from '../../../const/config.json'
+import "./Inventario.css";
 
 function Inventario() {
+  const data = [
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      disponible: "1",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      disponible: "1",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      disponible: "1",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+    {
+      comprador: "jairo Miller  Urrego",
+      cantidad: "20",
+      precio: "120.000",
+      fecha: "10/03/2024",
+    },
+  ];
   return (
     <>
-      <Header title={"Inventario"} />
-
-      <Search />
-
-      <div className="stock-genius-inventario-layoth">
-
-        <div className="stock-genius-inventario-layoth-left">
+      <div className="stock-genius-inventario-container">
+        <div className="stock-genius-inventario-header" style={{backgroundColor:config.backgroundPrincipal}}>
+          <Header title={"Inventario"} />
+          <Search />
+        </div>
+        <div className="stock-genius-inventario-layoth" >
+          <div className="stock-genius-inventario-layoth-left">
             <Mostrar />
 
-          <select className="stock-genius-options" id="opciones" name="opciones">
-            <option value="1">Disponible</option>
-            <option value="0">Fuera de Stock</option>
-          </select>
+            <select
+              className="stock-genius-options"
+              id="opciones"
+              name="opciones"
+            >
+              <option value="1">Disponible</option>
+              <option value="0">Fuera de Stock</option>
+            </select>
 
-
-          <Delete />
-        </div>
+            <Delete />
+          </div>
           <Icon icon={"add"} />
+        </div>
+        <div className="stock-genius-inventario-table">
+          <TableWithCheckbox data={data} />
+        </div>
+        <div className="stock-genius-inventario-total">
+          <span>TOTAL DEL INVENTARIO</span>
+        </div>
       </div>
-      <TableWithCheckbox />
     </>
-  )
+  );
 }
 
-export default Inventario
+export default Inventario;
