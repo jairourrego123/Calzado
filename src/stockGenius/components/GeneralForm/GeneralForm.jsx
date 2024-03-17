a2af6922d6826fa07fd409a4c8d614b65f40ad7c
JAIRO MILLER URREGO GARAY <jairourrego123@gmail.com>
2024-03-17 17:45:37 -0500

    General Form

---
+++ src/stockGenius/components/GeneralForm/GeneralForm.jsx
@@ -0,0 +1,36 @@
+import { useForm } from 'react-hook-form';
+import './GeneralForm.css'
+import config from '../../const/config.json'
+const GenericForm = ({ formFields, onSubmit,onClose }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className='stock-genius-component-general-form-container'>
+      {formFields.map((field) => (
+        <div key={field.name} className='stock-genius-component-general-form-content'>
+          <label htmlFor={field.name}>{field.label}:</label>
+          <input
+            type={field.type || 'text'}
+            id={field.name}
+            {...register(field.name, field.rules)}
+            className={errors[field.name] ? 'stock-genius-invalid-field' : 'stock-genius-component-general-form-content-input'}
+          />
+          {errors[field.name] && (
+            <span className="stock-genius-component-general-form-error-message">{errors[field.name].message}</span>
+          )}
+        </div>
+      ))}
+
+      <div className='stock-genius-component-general-form-buttons'>
+      <button className='stock-genius-component-general-form-button-cancelar stock-genius-button' type="button" onClick={onClose}>Cancelar</button>
+      <button className='stock-genius-component-general-form-button-submit stock-genius-button' type="submit" style={{backgroundColor:config.backgroundButton}}>Aceptar</button>
+      </div>
+    </form>
+  );
+};
+
+export default GenericForm;
