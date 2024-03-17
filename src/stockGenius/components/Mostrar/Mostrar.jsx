21a9310b983a649b34be7ca0da68c926dd2d89b7
JAIRO MILLER URREGO GARAY <jairourrego123@gmail.com>
2024-03-17 17:47:49 -0500

    component Mostrar

--- src/stockGenius/components/Mostrar/Mostrar.jsx
+++ src/stockGenius/components/Mostrar/Mostrar.jsx
@@ -5,12 +5,15 @@ function Mostrar() {
   return (
     
     <>
-      <label htmlFor="mostrar">Mostrar:</label>
+    <div>
+
+      <label htmlFor="mostrar">Mostrar: </label>
       <select className='stock-genius-options' id="mostrar" name="mostrar">
         {
           numbers.map((number, index) => <option key={index} value={number}>{number}</option>)
         }
       </select>
+        </div>
 
     </>
   )
