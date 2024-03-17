eb5097c905fe3c909261f9331571bfd3ea58e805
JAIRO MILLER URREGO GARAY <jairourrego123@gmail.com>
2024-03-17 17:46:54 -0500

    component general Modal

--- src/stockGenius/components/GeneralModal/GeneralModal.jsx
+++ src/stockGenius/components/GeneralModal/GeneralModal.jsx
@@ -1,14 +1,14 @@
 import Modal from 'react-modal';
 import './GeneralModal.css'; // Importa el archivo CSS
 
-function GeneralModal({ isOpen, onClose,icon,title, children }) {
+function GeneralModal({ isOpen, onClose,icon,title, layout, children }) {
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onClose}
       contentLabel="Modal"
       className="stock-genius-modal-content" // Agrega la clase al contenido del modal
-    //   overlayClassName="modal-overlay" // Agrega la clase a la capa que cubre la pantalla
+    //   overlay7ClassName="modal-overlay" // Agrega la clase a la capa que cubre la pantalla
       style={{
         overlay: {
           backgroundColor: 'rgba(0, 0, 0, 0.5)',
@@ -19,15 +19,19 @@ function GeneralModal({ isOpen, onClose,icon,title, children }) {
           left: '50%',
           transform: 'translate(-50%, -50%)',
           width: '500px',
-          height: '300px',
+        //   height: '300px',
+            
           borderRadius: '10px',
         },
       }}
     >
     <div className="stock-genius-component-modal-header">
-        <img src={`../../assets/icons/${icon}.svg`} alt="icon-modal" />
-        <h1>{title}</h1>
-        <button className="modal-close-button" onClick={onClose}>X</button>
+        <img src={`../../assets/icons/${icon}.svg`} alt="icon-modal" className='stock-genius-modal-icon'/>
+        <div className="stock-genius-component-modal-header-container">
+        <h1 className='stock-genius-titles stock-genius-component-modal-title'>{title}</h1>
+        <span className='stock-genius-component-modal-layout'>{layout}</span>
+        </div>
+        <img className="stock-genius-modal-close" src='../../assets/icons/close.svg' alt='icon-close' onClick={onClose}/>
     </div>
       {children}
     </Modal>
