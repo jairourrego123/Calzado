6a2673aa18d6f875dca513228771dac309328b72
JAIRO MILLER URREGO GARAY <jairourrego123@gmail.com>
2024-03-16 22:33:13 -0500

    page inventario

--- src/stockGenius/pages/Main/Inventario/Inventario.jsx
+++ src/stockGenius/pages/Main/Inventario/Inventario.jsx
@@ -6,6 +6,9 @@ import Delete from "../../../components/Delete/Delete";
 import Icon from "../../../components/Icon/Icon";
 import config from '../../../const/config.json'
 import "./Inventario.css";
+import { useState } from "react";
+import ModalAddProduct from "../../../components/ModalAddProduct/ModalAddProduct";
+import GeneralModal from "../../../components/GeneralModal/GeneralModal";
 
 function Inventario() {
   const data = [
@@ -74,35 +77,49 @@ function Inventario() {
       fecha: "10/03/2024",
     },
   ];
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
 
   return (
     <>
       <div className="stock-genius-inventario-container">
-        <div className="stock-genius-inventario-header" style={{backgroundColor:config.backgroundPrincipal}}>
+        <div className="stock-genius-inventario-header" style={{ backgroundColor: config.backgroundPrincipal }}>
           <Header title={"Inventario"} />
           <Search />
         </div>
         <div className="stock-genius-inventario-layoth" >
-          
-            <Mostrar />
 
-            <select
-              className="stock-genius-options"
-              id="opciones"
-              name="opciones"
-            >
-              <option value="1">Disponible</option>
-              <option value="0">Fuera de Stock</option>
-            </select>
+          <Mostrar />
 
-            <Delete />
+          <select
+            className="stock-genius-options"
+            id="opciones"
+            name="opciones"
+          >
+            <option value="1">Disponible</option>
+            <option value="0">Fuera de Stock</option>
+          </select>
+
+          <Delete />
           <button className="stock-genius-options stock-genius-button-export">Exportar</button>
-          <div className="stock-genius-inventario-add" style={{backgroundColor:config.backgroundPrincipal}}>
 
-          <Icon icon={"add"}  />
+          <div className="stock-genius-inventario-add" style={{ backgroundColor: config.backgroundPrincipal }} onClick={handleOpenModal}>
+
+            <Icon icon={"add"} />
           </div>
-          </div>
-        
+
+          <GeneralModal isOpen={isOpen} onClose={handleCloseModal} icon={"product"} title={"Nuevo Producto"}>
+          <ModalAddProduct/>
+          </GeneralModal>
+
+        </div>
+
         <div className="stock-genius-inventario-table">
           <TableWithCheckbox data={data} />
         </div>
