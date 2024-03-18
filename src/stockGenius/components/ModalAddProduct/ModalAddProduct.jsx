bee4a73caca39c4adb68f0268df1d730aa94c221
jairourrego123 <jairourrego123@gmail.com>
2024-03-18 16:00:27 -0500

    Modal add product

--- src/stockGenius/components/ModalAddProduct/ModalAddProduct.jsx
+++ src/stockGenius/components/ModalAddProduct/ModalAddProduct.jsx
@@ -1,17 +1,17 @@
 import GenericForm from "../GeneralForm/GeneralForm";
 
-function ModalAddProduct({onClose}) {
+function ModalAddProduct({onClose,product=[]}) {
   const onSubmit = (data) => {
     console.log(data);
     // Aquí puedes realizar acciones con los datos del formulario, como enviarlos a un servidor
   };
   const formFields = [
-    { name: 'referencia',  type : "text", label: 'Referencia*', rules: { required: 'Este campo es requerido' } },
-    { name: 'talla', type : "number", label: 'Talla*' ,rules: { required: 'Este campo es requerido', min: 1 } },
-    { name: 'color', type : "text",label: 'Color*' ,rules: { required: 'Este campo es requerido', min: 1 } } ,
-    { name: 'cantidad',type : "number", label: 'Cantidad*', rules: { required: 'Este campo es requerido', min: 1 } },
-    { name: 'stockMinimo', type : "number",label: 'Stock Mínimo*', rules: { required: 'Este campo es requerido', min: 1 } },
-    { name: 'valor', type : "number",label: 'Valor*', rules: { required: 'Este campo es requerido', min: 1 } },
+    { name: 'estilo',  value:product[0]?.estilo || '',type : "text", label: 'Estilo*', rules: { required: 'Este campo es requerido' } , },
+    { name: 'talla',value:product[0]?.talla || '', type : "number", label: 'Talla*' ,rules: { required: 'Este campo es requerido', min: 1 } },
+    { name: 'color',value:product[0]?.color || '', type : "text",label: 'Color*' ,rules: { required: 'Este campo es requerido', min: 1 } } ,
+    { name: 'cantidad',value:product[0]?.cantidad || '',type : "number", label: 'Cantidad*', rules: { required: 'Este campo es requerido', min: 1 } },
+    { name: 'stock_min',value:product[0]?.stock_min || '', type : "number",label: 'Stock Mínimo*', rules: { required: 'Este campo es requerido', min: 1 } },
+    { name: 'precio',value:product[0]?.precio || '', type : "number",label: 'Valor*', rules: { required: 'Este campo es requerido', min: 1 }},
   ];
 
   return (
