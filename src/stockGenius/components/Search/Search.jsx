1abaae8b387170abe2a38483960590e011bb95ff
jairourrego123 <jairourrego123@gmail.com>
2024-03-18 16:00:35 -0500

    component search

--- src/stockGenius/components/Search/Search.jsx
+++ src/stockGenius/components/Search/Search.jsx
@@ -1,14 +1,40 @@
+import { useState } from 'react'
 import './Search.css'
 
 
-function Search() {
+function Search({onSearch}) {
+  const [search, setSearch] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Evitar que se recargue la página
+    onSearch(search);
+  };
+
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
-    <div className="stock-genius-component-search" >
-      <img src={'../../assets/icons/search.svg'} alt='icono-buscar' className='stock-genius-component-search-icon-search'/>
-      <input type="text" placeholder="Buscar..."    className='stock-genius-component-search-input'/>
-      <img src={'../../assets/icons/arrow-right.svg'} alt='icono-flecha' className='stock-genius-component-search-icon-arrow'/>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="stock-genius-component-search">
+          <img src={'../../assets/icons/search.svg'} alt='icono-buscar' className='stock-genius-component-search-icon-search' />
+          <input 
+            type="text" 
+            placeholder="Buscar..."    
+            className='stock-genius-component-search-input' 
+            value={search} 
+            onChange={handleChange} 
+          />
+          
+            <img src={'../../assets/icons/arrow-right.svg'} alt='icono-flecha'  className="stock-genius-component-search-icon-arrow" onClick={handleSubmit}/>
+         
+        </div>
+      </form>
     </div>
-  )
+  );
 }
+
 
 export default Search
