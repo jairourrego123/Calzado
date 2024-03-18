070a482b625592bb5b3043715008e645b912a163
jairourrego123 <jairourrego123@gmail.com>
2024-03-18 16:00:11 -0500

    componente general de formularios

--- src/stockGenius/components/GeneralForm/GeneralForm.jsx
+++ src/stockGenius/components/GeneralForm/GeneralForm.jsx
@@ -18,6 +18,8 @@ const GenericForm = ({ formFields, onSubmit,onClose }) => {
             id={field.name}
             {...register(field.name, field.rules)}
             className={errors[field.name] ? 'stock-genius-invalid-field' : 'stock-genius-component-general-form-content-input'}
+            value={field.value}
+            disabled={field.disabled}
           />
           {errors[field.name] && (
             <span className="stock-genius-component-general-form-error-message">{errors[field.name].message}</span>
@@ -27,7 +29,7 @@ const GenericForm = ({ formFields, onSubmit,onClose }) => {
 
       <div className='stock-genius-component-general-form-buttons'>
       <button className='stock-genius-component-general-form-button-cancelar stock-genius-button' type="button" onClick={onClose}>Cancelar</button>
-      <button className='stock-genius-component-general-form-button-submit stock-genius-button' type="submit" style={{backgroundColor:config.backgroundButton}}>Aceptar</button>
+      <button className='stock-genius-component-general-form-button-submit stock-genius-button' type="submit" style={{backgroundColor:config.backgroundButton}}>Guardar</button>
       </div>
     </form>
   );
