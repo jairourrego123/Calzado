4e86245e44f022a989bfa76e252a792193746fce
jairourrego123 <jairourrego123@gmail.com>
2024-04-10 17:26:16 -0500

    formato precios

---
+++ src/stockGenius/components/Utilities/FormatPrice.js
@@ -0,0 +1,13 @@
+const FormatPrice = (price) => {
+    const formatter = new Intl.NumberFormat('es-CO', {
+      style: 'currency',
+      currency: 'COP',
+      minimumFractionDigits: 0
+    });
+    
+    if(typeof(price)!='number') price = parseInt(price?.replace(/[$\.]/g, ''))
+    if (isNaN(price)) price = 0;
+    return formatter.format(price);
+  };
+
+export default FormatPrice
