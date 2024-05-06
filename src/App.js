5a71aba12183dbafca9b84e5375898eb74306ff8
JAIRO MILLER URREGO GARAY <jairourrego123@gmail.com>
2024-05-05 21:52:53 -0500

    Cambio de rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import StockGenius from './stockGenius/StockGenius';
 
 function App() {
   return (
    <Routes>
+    <Route path='/' element={<Navigate to={"/stock-genius/login"}/> }/>
     <Route path='/stock-genius/*' element={<StockGenius/>}/>
    </Routes>
   );
