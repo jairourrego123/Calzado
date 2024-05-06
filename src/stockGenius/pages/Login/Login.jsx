0c4e8cb6341498e7c502c5827c6a1401f76f3a30
JAIRO MILLER URREGO GARAY <jairourrego123@gmail.com>
2024-05-05 21:56:25 -0500

    Login

--- src/stockGenius/pages/Login/Login.jsx
+++ src/stockGenius/pages/Login/Login.jsx
@@ -6,23 +6,27 @@ function Login() {
 
 
   return (
-    <div className='stock-genius-login'>
-      <div className="stock-genius-login-content">
-        <h1 className='stock-genius-login-title'>Login</h1>
-        <span>Bienvenido de vuelta! ingresa para acceder a tu base de datos</span>
-        <LoginForm/>
+    <>
+
+      <div className='stock-genius-login'>
+        <div className="stock-genius-login-content">
+          <span className='stock-genius-login-title'>Login</span>
+          <span className='stock-genius-login-layout'>Bienvenido de vuelta! ingresa para acceder a tu base de datos</span>
+          <LoginForm />
+        </div>
+
+
+
+
       </div>
-        <svg className="stock-genius-login-vector-izq" width="854" height="364" viewBox="0 0 854 364" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M429.5 176.484C241 180.984 87.5 -15.5158 -21.5 0.984153L-94 451.984C253.667 428.151 928.6 376.484 847 360.484C745 340.484 618 171.984 429.5 176.484Z" fill="#191F2F" />
-        </svg>
-        <svg className="stock-genius-login-vector-der" width="1214" height="335" viewBox="0 0 1214 335" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M465.5 115.5C260 -0.999985 79 58.5 0.5 -2.99998C404 -27.1666 1212.4 -75.3 1218 -74.5C1225 -73.5 1364 341 1236 334C1108 327 671 232 465.5 115.5Z" fill="#191F2F" />
-        </svg>
-      
 
-
-
-    </div>
+      <svg className="stock-genius-login-vector-izq" width="854" height="364" viewBox="0 0 854 364" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M429.5 176.484C241 180.984 87.5 -15.5158 -21.5 0.984153L-94 451.984C253.667 428.151 928.6 376.484 847 360.484C745 340.484 618 171.984 429.5 176.484Z" fill="#191F2F" />
+      </svg>
+      <svg className="stock-genius-login-vector-der" width="1214" height="335" viewBox="0 0 1214 335" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M465.5 115.5C260 -0.999985 79 58.5 0.5 -2.99998C404 -27.1666 1212.4 -75.3 1218 -74.5C1225 -73.5 1364 341 1236 334C1108 327 671 232 465.5 115.5Z" fill="#191F2F" />
+      </svg>
+    </>
   )
 }
 
--- src/stockGenius/pages/Login/components/LoginForm/LoginForm.jsx
+++ src/stockGenius/pages/Login/components/LoginForm/LoginForm.jsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import './LoginForm.css'
 import { useForm } from 'react-hook-form'
 import Icon from "../../../../components/Icon/Icon";
-
+import { useNavigate } from 'react-router-dom';
+import config from '../../../../const/config.json'
 function LoginForm() {
-    const { register, handleSubmit, errors } = useForm();
+    const { register, handleSubmit, formState: { errors }, } = useForm();
     const [showPassword, setShowPassword] = useState(false);
-
+    const navigate = useNavigate()
 
     const onSubmit = (e)=>{
       console.table(e)
+      navigate(config.routerPrincipal+"/home",{replace:true})
     }
   return (
    
@@ -22,17 +24,22 @@ function LoginForm() {
                     type="text"
                     id="username"
                     name="username"
+                    required
                     placeholder='Usuario'
                     {...register("username",{ required: 'Ingrese su nombre de usuario' })}
                 />
                 {/* {errors.username && <span>{errors.username.message}</span>} */}
             </div>
+            {errors['username'] && (
+            <span className="stock-genius-component-general-form-error-message">{errors.username.message}</span>
+          )}
 
             <div className='stock-genius-login-form-password'>
                 <input
                     type={showPassword ? 'text' : 'password'}
                     id="password"
                     name="password"
+                    required
                     placeholder='Contraseña'
                      {...register("password",{ required: 'Ingrese su contraseña' })}
                      
@@ -42,6 +49,9 @@ function LoginForm() {
                 <Icon icon={showPassword?"visibility":"visibility_off"}/>
                 </div>
             </div>
+            {errors['password'] && (
+            <span className="stock-genius-component-general-form-error-message">{errors.password.message}</span>
+          )}
 
             <button  className="stock-genius-login-form-submit"type="submit">Iniciar sesión</button>
         </div>
