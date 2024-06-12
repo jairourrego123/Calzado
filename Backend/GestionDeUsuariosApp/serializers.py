080219a04c5d60f58a133b794c4a9ec0450dbe91
JAIRO MILLER URREGO GARAY <jairourrego123@gmail.com>
2024-06-11 22:08:08 -0500

    Serializer de modelos

--- Backend/GestionDeUsuariosApp/serializers.py
+++ Backend/GestionDeUsuariosApp/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
-from .models import Usuario
+from .models import Usuario , Tenant
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
+        fields = '__all__'
+class TenantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tenant
         fields = '__all__'
