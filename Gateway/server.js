79e5c95dd0d0e620d76747493a112c4b77bd9928
jairourrego123 <jairourrego123@gmail.com>
2024-06-13 17:30:48 -0500

    cors server

--- Gateway/server.js
+++ Gateway/server.js
@@ -4,12 +4,27 @@ const cors = require('cors'); // Habilita CORS
 
 const app = express();
 const apiRouter = require('./routers/apiRouter');
-app.use(cors()); // Habilita CORS para todas las rutas
+app.use(cors({
+  origin: 'http://localhost:3000', // unicamente al dominio de tu frontend
+  credentials: true
+}));
 app.use(express.json()); // Habilita el análisis del cuerpo de la solicitud JSON
 
-app.use('/api', apiRouter);
 
-const PORT = process.env.PORT || 3000;
+// Middleware para verificar el referer/origin
+app.use((req, res, next) => {
+  console.log(req.headers.referer);
+  const allowedOrigins = ['http://localhost']; //  dominio de del frontend
+  const origin = req.headers.origin || req.headers.referer;
+  
+  if (allowedOrigins.includes(origin)) {
+    next();
+  } else {
+    res.status(403).send('Forbidden');
+  }
+});
+app.use('/api', apiRouter);
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`API Gateway escuchando en el puerto ${PORT}`);
 });
