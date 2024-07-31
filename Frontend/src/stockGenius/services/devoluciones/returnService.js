d90732b728ddf52a4de4bf9ac9d19353609c234c
jairourrego123 <jairourrego123@gmail.com>
2024-07-31 11:40:25 -0500

    Servicio obtener motivos de devolucion

--- Frontend/src/stockGenius/services/devoluciones/returnService.js
+++ Frontend/src/stockGenius/services/devoluciones/returnService.js
@@ -5,25 +5,21 @@ const getReturns = async (params={}) => {
   const response = await apiClient.get('/devoluciones/devoluciones/',params);
   return response.data;
 };
+const getTypeReturns = async (params={}) => {
+  const response = await apiClient.get('/devoluciones/motivos/',params);
+  return response.data;
+};
 
-// const addReturn = async (returnItem) => {
-//   const response = await apiClient.post('/devoluciones/', returnItem);
-//   return response.data;
-// };
 
 const addReturn = async (returnItem) => {
   const response = await apiClient.post('/devoluciones/crear/devolucion/', returnItem);
   return response.data;
 };
 
-const updateReturn = async (id, returnItem) => {
-  const response = await apiClient.patch(`/devoluciones/${id}/`, returnItem);
-  return response.data;
-};
 
 const deleteReturn = async (id) => {
   const response = await apiClient.delete(`/devoluciones/${id}/`);
   return response.data;
 };
 
-export { getReturns, addReturn, updateReturn, deleteReturn };
+export { getReturns, addReturn, getTypeReturns, deleteReturn };
